33deb2e0dc42f4d787e09efa19e4cd308d6815cb
IidaPeltonen <n0peii00@students.oamk.fi>
2022-11-07 14:16:35 +0200

    API-rajapinta avattu tuntihinta-tietoaineistoon

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+//testi, hakee tuntihinnastosta hinnan 1.11 10-11
+const URL = 'https://api.fingrid.fi/v1/variable/%09317/events/json?start_time=2022-11-01T10%3A00%3A00Z&end_time=2022-11-01T11%3A00%3A00Z';
+const APIKEY = 'D1fcMx4D0O6ua4wwnEGHZ9re1lFBsREE5G2tNZ8D';
+
+//https://api.fingrid.fi/v1/variable/ tämä alku on aina sama
+//%09317/events/json? tämä pätkä muuttuu, tässä tapauksessa meidän käyttämä "taulukko" on 317, eli variableId on 318
+//start_time=2022-11-01T10%3A00%3A00Z&end_time=2022-11-01T11%3A00%3A00Z'; tässä on sitten se aikaväli, miltä hinta haetaan
+
 
 export default function App() {
+  const [data, setData] = useState('')
+
+  useEffect(() => {
+    fetch (URL, {
+      headers: {
+        'method': 'GET',
+        'x-api-key': APIKEY
+      }
+    })
+
+    .then(response => response.json())
+    .then((json) => {
+      console.log(json)
+      setData(json)
+    }, (error) => {
+      console.log(error)
+    })
+  })
+     
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+      <Text>jjiksrjnjt</Text>
+
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,3 +47,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
