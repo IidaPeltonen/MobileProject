ec656d092af711b4ab6d2394478dc637b01e84e8
adepade <n7haat00@students.oamk.fi>
2022-11-07 18:59:19 +0200

    testi

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 //testi, hakee tuntihinnastosta hinnan 1.11 10-11
-const URL = 'https://api.fingrid.fi/v1/variable/%09317/events/json?start_time=2022-11-01T10%3A00%3A00Z&end_time=2022-11-01T11%3A00%3A00Z';
-const APIKEY = 'D1fcMx4D0O6ua4wwnEGHZ9re1lFBsREE5G2tNZ8D';
+const URL = 'https://api.fingrid.fi/v1/variable/%09317/events/json?start_time=2022-11-01T10%3A07%3A21-07%3A00&end_time=2022-11-02T11%3A07%3A21-07%3A00';
+const APIKEY = 'BRMbElnXrT3oySHJz4Eas16LjweZiaPt9dfcRQAS';
 
 //https://api.fingrid.fi/v1/variable/ tämä alku on aina sama
 //%09317/events/json? tämä pätkä muuttuu, tässä tapauksessa meidän käyttämä "taulukko" on 317, eli variableId on 318
@@ -33,7 +33,7 @@ export default function App() {
     }, (error) => {
       console.log(error)
     })
-  })
+  }, []);
      
   return (
 
