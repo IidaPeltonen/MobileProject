6f19d163552735a60c8a92f1106533e160105a39
IidaPeltonen <n0peii00@students.oamk.fi>
2022-11-09 19:28:12 +0200

    osoite jaettu palasiin

--- App.js
+++ App.js
@@ -3,40 +3,37 @@ import { StyleSheet, Text, View } from 'react-native';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-
-//testi, entsoeu
 const URL2 = 'https://web-api.tp.entsoe.eu/api?securityToken=';
 const APIKEY2 = '4d24ca50-7859-4d0d-97c2-de16d61007af';
-const loppu = '&documentType=A65&processType=A16&outBiddingZone_Domain=10YCZ-CEPS-----N&periodStart=201512312300&periodEnd=201612312300'
+const documentType = '&documentType=A44&' //mitä tietoaineistoa luetaan
+const in_Domain = 'in_Domain=10YFI-1--------U&' // maakoodi
+const out_Domain = 'out_Domain=10YFI-1--------U&'
+const start = 'periodStart=202211082300&'
+const end = 'periodEnd=202211092300'
+//const loppu = '&documentType=A44&in_Domain=10YFI-1--------U&out_Domain=10YFI-1--------U&periodStart=202211082300&periodEnd=202211092300'
 
 export default function App() {
   const [data, setData] = useState([])
 
- // GET /api?securityToken=TOKEN (other parameters omitted)
-
- //https://web-api.tp.entsoe.eu/api?securityToken=MYTOKEN&documentType=A65&processType=A16&outBiddingZone_Domain=10YCZ-CEPS-----N&periodStart=201512312300&periodEnd=201612312300
-
-
-
   useEffect(() => {
-    fetch (URL2 + APIKEY2 + loppu ,{
+    fetch (URL2 + APIKEY2 + documentType + in_Domain + out_Domain + start + end ,{
       headers: {
         'method': 'GET',
-        'securityToken': APIKEY2,
-        'DocumentType': 'A44',
-        'Content-Type': 'application/xml'
+        'Content-Type': 'application/xml',
       },
-      parameter: {
+/*       parameter: {
         //'TimeInterval': '2016-01-01T00:00Z/2016-01-02T00:00Z',
-        'periodStart': '201601010000',
-        'periodEnd': '201701010000',
-      }
+        'documentType':'&documentType=A44&',
+        'in_Domain': 'in_Domain=10YFI-1--------U&',
+        'out_Domain': 'out_Domain=10YFI-1--------U&',
+        'periodStart': '202211082300&',
+        'periodEnd': '202211092300',
+      } */
     })
 
     .then(response => response.xml())
     .then((xml) => {
       console.log(xml)
-    
       setData(xml)
     }, (error) => {
       console.log(error)
