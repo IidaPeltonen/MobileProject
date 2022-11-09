483ad0eaa145a85ee48e887f5d2c179e8c2749f3
IidaPeltonen <n0peii00@students.oamk.fi>
2022-11-09 18:20:01 +0200

    uusi api käyttöön

--- App.js
+++ App.js
@@ -3,34 +3,41 @@ import { StyleSheet, Text, View } from 'react-native';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-//testi, hakee tuntihinnastosta hinnan 1.11 10-11
-const URL = 'https://api.fingrid.fi/v1/variable/%09317/events/json?start_time=2022-11-01T10%3A07%3A21-07%3A00&end_time=2022-11-02T11%3A07%3A21-07%3A00';
-const APIKEY = 'BRMbElnXrT3oySHJz4Eas16LjweZiaPt9dfcRQAS';
 
-//https://api.fingrid.fi/v1/variable/ tämä alku on aina sama
-//%09317/events/json? tämä pätkä muuttuu, tässä tapauksessa meidän käyttämä "taulukko" on 317, eli variableId on 318
-//start_time=2022-11-01T10%3A00%3A00Z&end_time=2022-11-01T11%3A00%3A00Z'; tässä on sitten se aikaväli, miltä hinta haetaan
-
-
-//jos haluaa ajan NYT, tai tietyllä hetkellä, sekä alku, että loppuaika pitää olla samat, muoto on 
-//siis 2022-11-07T13:00:00Z
+//testi, entsoeu
+const URL2 = 'https://web-api.tp.entsoe.eu/api?securityToken=';
+const APIKEY2 = '4d24ca50-7859-4d0d-97c2-de16d61007af';
+const loppu = '&documentType=A65&processType=A16&outBiddingZone_Domain=10YCZ-CEPS-----N&periodStart=201512312300&periodEnd=201612312300'
 
 export default function App() {
   const [data, setData] = useState([])
 
+ // GET /api?securityToken=TOKEN (other parameters omitted)
+
+ //https://web-api.tp.entsoe.eu/api?securityToken=MYTOKEN&documentType=A65&processType=A16&outBiddingZone_Domain=10YCZ-CEPS-----N&periodStart=201512312300&periodEnd=201612312300
+
+
+
   useEffect(() => {
-    fetch (URL, {
+    fetch (URL2 + APIKEY2 + loppu ,{
       headers: {
         'method': 'GET',
-        'x-api-key': APIKEY
+        'securityToken': APIKEY2,
+        'DocumentType': 'A44',
+        'Content-Type': 'application/xml'
+      },
+      parameter: {
+        //'TimeInterval': '2016-01-01T00:00Z/2016-01-02T00:00Z',
+        'periodStart': '201601010000',
+        'periodEnd': '201701010000',
       }
     })
 
-    .then(response => response.json())
-    .then((json) => {
-      console.log(json)
+    .then(response => response.xml())
+    .then((xml) => {
+      console.log(xml)
     
-      setData(json)
+      setData(xml)
     }, (error) => {
       console.log(error)
     })
