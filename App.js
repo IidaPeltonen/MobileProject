02eb9ae1ae60200f2ce496c0fffad70b49584ebe
IidaPeltonen <n0peii00@students.oamk.fi>
2022-11-14 19:24:32 +0200

    kommenttia lisätty

--- App.js
+++ App.js
@@ -3,51 +3,46 @@ import { StyleSheet, Text, View } from 'react-native';
 import { useState, useEffect } from 'react';
 import XMLParser from 'react-xml-parser';
 
-const URL2 = 'https://web-api.tp.entsoe.eu/api?securityToken=';
-const APIKEY2 = '4d24ca50-7859-4d0d-97c2-de16d61007af';
+const APIKEY = '4d24ca50-7859-4d0d-97c2-de16d61007af';
 const documentType = '&documentType=A44&' //mitä tietoaineistoa luetaan
 const in_Domain = 'in_Domain=10YFI-1--------U&' // maakoodi
 const out_Domain = 'out_Domain=10YFI-1--------U&'
-const start = 'periodStart=' + year + month + day + StartTime + '&'
-const end = 'periodEnd=' + year + month + day + EndTime + '&'
-const year= new Date().getFullYear()
-const month = new Date().getMonth()
-const day = new Date().getDay()
-const time = new Date().getHours() + '' + new Date().getMinutes()
+const year = new Date().getFullYear()
+const month = new Date().getMonth() + 1
+const day = new Date().getDate()
 const StartTime = '0000'
 const EndTime = '2359'
+const start = 'periodStart=' + year + month + day + StartTime + '&'
+const end = 'periodEnd=' + year + month + day + EndTime + '&'
+
+const URL = 'https://web-api.tp.entsoe.eu/api?securityToken=' + APIKEY + documentType + in_Domain + out_Domain
+  + start + end
+const time = new Date().getHours() + '' + new Date().getMinutes() // current time, tunti, täatä pitää vähentää 1, jota saadaan
+    //oikea indeksi taulukosta
 
 export default function App() {
   const [data, setData] = useState([])
   const [prices, setPrices] = useState([]);
-  const [date, setDate] = useState(new Date())
-  console.log(time)
 
   useEffect(() => {
-    fetch(URL2 + APIKEY2 + documentType + in_Domain + out_Domain + start + end, {
+    fetch(URL , {
       headers: {
         'method': 'GET',
         'Content-Type': 'application/xml',
       },
     })
-/*       .then(response => response.text())
-      .then((textResponse) => console.log('response is ', textResponse))
-      .catch((error) => {
-        console.log(error)
+      .then(res => res.text())
+      .then(data => {
+        let json = new XMLParser().parseFromString(data);
+        console.log(json.getElementsByTagName('price'));
+        setPrices(json.getElementsByTagName('price'));
+        //console.log('hinnat' + prices[0].value); //megawattituntihinta, pitää muutta kilowattitunneiksi ja lisätä alv
+        console.log(json)
+        setData(json)
+        // console.log('datan sisältö: ' + data)
       })
-  }, []); */
-  .then(res => res.text())
-  .then(data => {
-      let json = new XMLParser().parseFromString(data);
-      //console.log(json.getElementsByTagName('price'));
-      setPrices(json.getElementsByTagName('price'));
-      console.log('hinnat' + prices[0].value); //megawattituntihinta, pitää muutta kilowattitunneiksi ja lisätä alv
-    //  console.log(json)
-      setData(json)
-     // console.log('datan sisältö: ' + data)
-  })
-  .catch(err => console.log(err));
-}, [])
+      .catch(err => console.log(err));
+  }, [])
 
   return (
     <View style={styles.container}>
