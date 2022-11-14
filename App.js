c4cb660860ebd66f3e81ed49699d3bc4d20671fb
IidaPeltonen <n0peii00@students.oamk.fi>
2022-11-14 14:35:37 +0200

    xml -> json ehkä toimii?

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useState, useEffect } from 'react';
-import axios from 'axios';
+import XMLParser from 'react-xml-parser';
 
 const URL2 = 'https://web-api.tp.entsoe.eu/api?securityToken=';
 const APIKEY2 = '4d24ca50-7859-4d0d-97c2-de16d61007af';
@@ -16,26 +16,33 @@ export default function App() {
   const [data, setData] = useState([])
 
   useEffect(() => {
-    fetch (URL2 + APIKEY2 + documentType + in_Domain + out_Domain + start + end ,{
+    fetch(URL2 + APIKEY2 + documentType + in_Domain + out_Domain + start + end, {
       headers: {
         'method': 'GET',
         'Content-Type': 'application/xml',
       },
     })
+/*       .then(response => response.text())
+      .then((textResponse) => console.log('response is ', textResponse))
+      .catch((error) => {
+        console.log(error)
+      })
+  }, []); */
+  .then(res => res.text())
+  .then(data => {
+      let json = new XMLParser().parseFromString(data); 
+      console.log(json)
+      setData(json)
+      console.log('datan sisältö: ' + data)
+  })
+  .catch(err => console.log(err));
+}, [])
 
-    .then(response => response.text())
-    .then((textResponse) =>  console.log('response is ', textResponse)) 
-    .catch((error) => {
-      console.log(error)
-    })
-
-  }, []);
-     
   return (
-    <View style={styles.container}> 
-    {
-      data && data.length>0 && data.map((item)=><Text>{item.value}</Text>)
-    }
+    <View style={styles.container}>
+      {
+        data && data.length > 0 && data.map((item) => <Text>{item.value}</Text>)
+      }
     </View>
   );
 }
