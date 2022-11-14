df47f0c1af06029b5b7bd8103f858ebc645889e7
IidaPeltonen <n0peii00@students.oamk.fi>
2022-11-14 20:58:51 +0200

    nyt se hinta näkyy oikein!

--- App.js
+++ App.js
@@ -13,13 +13,12 @@ const day = new Date().getDate()
 const StartTime = '0000'
 const EndTime = '0000'
 const start = 'periodStart=' + year + month + day + StartTime + '&'
-const end = 'periodEnd=' + year + month + (day+1) + EndTime 
+const end = 'periodEnd=' + year + month + (day + 1) + EndTime
 
 const URL = 'https://web-api.tp.entsoe.eu/api?securityToken=' + APIKEY + documentType + in_Domain + out_Domain
   + start + end
-const time = new Date().getHours() // current time, tunti, täatä pitää vähentää 1, jota saadaan
-    //oikea indeksi taulukosta
-const index = time -1 // hakee indexin,josta tämän hetken hinta haetaan
+const time = new Date().getHours() // current time, tunti
+const index = time -1 // tästä tauluko nindeksistä hartaan hinta
 
 export default function App() {
   const [data, setData] = useState([])
@@ -27,7 +26,7 @@ export default function App() {
   const [priceNow, setPriceNow] = useState('');
 
   useEffect(() => {
-    fetch(URL , {
+    fetch(URL, {
       headers: {
         'method': 'GET',
         'Content-Type': 'application/xml',
@@ -38,21 +37,26 @@ export default function App() {
         let json = new XMLParser().parseFromString(data);
         //console.log(json.getElementsByTagName('price'));
         setPrices(json.getElementsByTagName('price'))
-        let sum = parseFloat((prices[index].value) / 10 * 1.24).toFixed(2) // alv nyt, ennen 1.12.22
+        let noAlv = parseFloat((prices[index].value) / 10).toFixed(2) 
+        let sum = parseFloat(noAlv * 1.24).toFixed(2) // alv nyt, ennen 1.12.22
+        let noAlv2 = parseFloat((prices[0].value) / 10).toFixed(2) 
+        let sum2 = parseFloat(noAlv * 1.24).toFixed(2) // alv nyt, ennen 1.12.22
         setPriceNow(sum)
-        console.log('Hinta nyt, sis alv: ' + sum + 's/kWh')
-        //console.log('hinta nyt: ' + prices[index].value); //megawattituntihinta, pitää muutta kilowattitunneiksi ja lisätä alv
-        //console.log('indeksi, josta hinta haetaan: ' + index)  
+        console.log('Taulukon aloitusaika: ' + start)
+        console.log('Taulukon lopetusaika: ' + end)
+        console.log('Kellonaika haettaessa: ' + time)
+        console.log('Haettava indeksi: ' + index)
+        console.log('Hinta nyt, ei sis  alv: ' + noAlv + 'snt/kWh')
+        console.log('Hinta nyt, sis alv: ' + sum + 'snt/kWh')
+        console.log('Testi, hinta taulukon indeksistä 0, ei sis  alv: ' + noAlv2 + 's/kWh')
+        console.log('Testi, hinta taulukon indeksistä 0, sis alv: ' + sum2 + 's/kWh')
       })
       .catch(err => console.log(err));
-  }, [])
+    }, [])
 
   return (
     <View style={styles.container}>
-      {
-        data && data.length > 0 && data.map((item) => <Text>{item.value}</Text>)
-      }
-<Text>Hinta nyt: {priceNow} </Text>
+      <Text>Hinta nyt: {priceNow} snt/kWh (sis. Alv 24%)</Text>
     </View>
   );
 }
@@ -65,4 +69,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+
 
