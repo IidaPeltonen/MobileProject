ef239bf45681803cc87cf2974388f70184276a37
Ville-r <81159672+Ville-r@users.noreply.github.com>
2022-11-20 13:52:27 +0200

    kulutus ja tuotanto päivitystä

--- components/Eleproduce.js
+++ components/Eleproduce.js
@@ -5,9 +5,12 @@ import XMLParser from 'react-xml-parser';
 import styles from "../style/style"
 
 const APIKEY = '4d24ca50-7859-4d0d-97c2-de16d61007af';
-const documentType = '&documentType=A65' //mitä tietoaineistoa luetaan, "system total load"
+const documentType = '&documentType=A65' //mitä tietoaineistoa luetaan, "system total load" KULUTUSTIETO
+const documentType2 = '&documentType=A71' //mitä tietoaineistoa luetaan, "system total load" KULUTUSTIETO
 const outBiddingZone_Domain = '&outBiddingZone_Domain=10YFI-1--------U' // maakoodi, "FI"
+const in_Domain = '&in_Domain=10YFI-1--------U' // maakoodi, "FI"
 const processType = '&processType=A16' // minkä tyyppistä kulutustietoa haetaan, "realised"
+const processType2 = '&processType=A01' // minkä tyyppistä tuotantotietoa haetaan, "day ahead"
 const year = new Date().getFullYear()
 const month = new Date().getMonth() + 1
 const day = new Date().getDate()
@@ -21,14 +24,26 @@ const index = time - 2
 // tällä saa Suomen toteutuneen kokonaiskulutuksen tuntitasolla kuluvalta päivältä
 const URL = 'https://web-api.tp.entsoe.eu/api?securityToken=' + APIKEY + documentType + processType + outBiddingZone_Domain + start + end
 // tällä alla olevalla osoitteella tulee suunniteltu tuotanto päivän ajalta suomessa.
-const URL2 = 'https://web-api.tp.entsoe.eu/api?securityToken=4d24ca50-7859-4d0d-97c2-de16d61007af&documentType=A71&processType=A01&in_Domain=10YFI-1--------U&periodStart=202211180000&periodEnd=202211182300'
+const URL2 = 'https://web-api.tp.entsoe.eu/api?securityToken=' + APIKEY + documentType2 + processType2 + in_Domain + start + end
 
 export default function Footer() {
 
-  const [loads, setLoads] = useState([]);
-  const [lastLoad, setLastLoad] = useState('');
-  const [generations, setGenerations] = useState([]);
-  const [lastGeneration, setLastGeneration] = useState('');
+  const [loads, setLoads] = useState([]); // taulukko kulutustiedoille
+  const [lastLoad, setLastLoad] = useState(''); //viimeisin toteutunut kokonaiskulutus
+  const [generations, setGenerations] = useState([]); //taulukko tuotantoluvuille
+  const [lastGeneration, setLastGeneration] = useState(''); //ennustettu kokonaistuotanto
+  const [importNeed, setImportNeed] = useState(''); // muuttuja tuontisähkön tarpeelle
+
+  // funktio tuntisähkön tarpeen laskentaan
+  function importNeedCalculation(lastLoad,lastGeneration) {
+    let situation = lastLoad - lastGeneration
+
+    if (situation >= 0) {
+      setImportNeed(Number(situation));
+    } else {
+      setImportNeed(Number(0));
+    }
+  }
 
   useEffect(() => {
     fetch(URL, {
@@ -62,22 +77,23 @@ export default function Footer() {
         setGenerations(json.getElementsByTagName('quantity'))
 
       // tällä tavalla saa ulos tietyn tunnin suunnitellun kokonaistuotannon.
-      //  console.log(generations[16].value) 
-        setLastGeneration(Number(generations[16].value));
+        console.log(generations[16].value) 
+        setLastGeneration(Number(generations[index].value));
+        importNeedCalculation(lastLoad,lastGeneration)
       })
       .catch(err => console.log(err));
   }, [])
-
   return (
     <View>
       <ScrollView>
         <View style={styles.square}>
-      <Text style = {styles.title}>Sähkön kokonaiskulutus ja -tuotanto</Text>
-      <Text style={styles.text}>Kokonaiskulutus Suomessa kello {index} - {index + 1} (MWh/h):</Text>
-      <Text style={styles.important}>{lastLoad}</Text>
-      <Text style={styles.text}>Suunniteltu kokonaistuotanto Suomessa edellisen tunnin aikana (MWh/h): </Text>
-      <Text style={styles.important}>{lastGeneration}</Text>
-      <Text></Text>
+          <Text style={styles.title}>Sähkön kokonaiskulutus ja -tuotanto Suomessa kello {index} - {index + 1} (MWh/h)</Text>
+          <Text style={styles.text}>Toteutunut kokonaiskulutus</Text>
+          <Text style={styles.important}>{lastLoad}</Text>
+          <Text style={styles.text}>Suunniteltu kokonaistuotanto</Text>
+          <Text style={styles.important}>{lastGeneration}</Text>
+          <Text style={styles.text}>Laskennallinen tuontisähkön tarve</Text>
+          <Text style={styles.important}>{importNeed}</Text>
         </View>
       </ScrollView>
     </View>
